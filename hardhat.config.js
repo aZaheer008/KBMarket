6c69278b1d7cda8cbf172899b5ce3a0845c288c8
aZaheer008 <kmitinstitute008@gmail.com>
2022-08-09 11:18:13 +0500

    configuration setting

---
+++ hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomicfoundation/hardhat-toolbox");
+const projectId = '3c07677484124b5ca360bad84fa878d7';
+const fs = require('fs');
+const keyData = fs.readFileSync('./p-key.txt',{
+  encoding: 'utf-8', flag:'r'
+});
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork : 'hardhat',
+  networks : {
+    hardhat : {
+      chainId : 1337 // config standard
+    },
+    ropsten : {
+      url:`https://ropsten.infura.io/v3/${projectId}`,
+      accounts:[keyData]
+    }
+  },
+  solidity: {
+    version : "0.8.9",
+    settings : {
+      optimizer: {
+        enabled: true,
+        runs : 200
+      }
+    }
+  }
+};
